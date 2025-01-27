07edb5a82810d0d60d6ad2e0eea9e58bff1b99e4
ReicaD <dbamanya447@gmail.com>
2025-01-27 17:57:35 -0500

    modified with javascript

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-const todoForm= document.querySelector('form');
-const todoInput = document.getElementById('todo-input')
-const todoListUL = document.getElementById('todo-list')
+document.addEventListener('DOMContentLoaded', function () {
+    const todoForm = document.querySelector('form');
+    const todoInput = document.getElementById('todo-input');
+    const todoListUL = document.getElementById('todo-list');
+
+    let allTodos = [];
+    // This is for the submit event
+    todoForm.addEventListener('submit', function (e) {
+        e.preventDefault(); // Prevent the default form submission
+        alert("test");
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
