b7cae04d39be7f7176ce2295687d1e25bcba4ebb
ReicaD <dbamanya447@gmail.com>
2025-01-29 02:27:02 -0500

    modified with jsx function (input)

--- app.js
+++ app.js
@@ -1,55 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const todoForm = document.querySelector('form');
-    const todoInput = document.getElementById('todo-input');
-    const todoListUL = document.getElementById('todo-list');
+document.addEventListener("DOMContentLoaded", function () {
+  const todoForm = document.querySelector("form");
+  const todoInput = document.getElementById("todo-input");
+  const todoListUL = document.getElementById("todo-list");
 
-    let allTodos = [];
-    // This is for the submit event
-    todoForm.addEventListener('submit', function (e) {
-        e.preventDefault(); // Prevent the default form submission
-        addTodo(); 
-    });
+  let allTodos = [];
+  // This is for the submit event
+  todoForm.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission
+    addTodo();
+  });
 
-    function addTodo(){
-        const todoText = todoInput.value.trim();
-        //to present user from submiting an empy todo
-        if (todoText.length > 0){
-
-            allTodos.push(todoText);
-            valueInput.value = "";
-        }
-        // alert(todoText);
-        //  console.log(allTodos);
+  function addTodo() {
+    const todoText = todoInput.value.trim();
+    //to present user from submiting an empy todo
+    if (todoText.length > 0) {
+      allTodos.push(todoText);
+      todoInput.value = "";
     }
-})
+    // alert(todoText);
+    //  console.log(allTodos);
+  }
+  //updating the submited value of the todo list
+  function updateTodoList(){
+      todoListUL.innerHTML = "";
+  }
+  function createTodoItem(todo){
+      const todoLI = document.createElement("li");
+      todoLI.innerText = todo;
+      todoListUL.append(todoLI);
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const todoForm= document.querySelector('form');
-// const todoInput = document.getElementById('todo-input')
-// const todoListUL = document.getElementById('todo-list')
-
-// let allTodos =[];
-// // this is for the submit to be excuted by also adding the call back
-// todoForm.addEventListener('submit', function(e){
-//     alert("test");
-// })
+ 
