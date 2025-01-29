75e77d2f36f64a9e58855421481bdd14ca6a6959
ReicaD <dbamanya447@gmail.com>
2025-01-29 01:50:47 -0500

    modified with JSX

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ document.addEventListener('DOMContentLoaded', function () {
     // This is for the submit event
     todoForm.addEventListener('submit', function (e) {
         e.preventDefault(); // Prevent the default form submission
-        alert("test");
+        addTodo(); 
     });
 
     function addTodo(){
-        const todoText = todoInput.ariaValueMax.trim();
+        const todoText = todoInput.value.trim(); 
         allTodos.push(todoText)
-        console.log(allTodos);
-
+        alert(todoText);
+        // console.log(allTodos);
     }
-});
+})
 
 
 
@@ -39,4 +39,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
- 
+// const todoForm= document.querySelector('form');
+// const todoInput = document.getElementById('todo-input')
+// const todoListUL = document.getElementById('todo-list')
+
+// let allTodos =[];
+// // this is for the submit to be excuted by also adding the call back
+// todoForm.addEventListener('submit', function(e){
+//     alert("test");
+// })
