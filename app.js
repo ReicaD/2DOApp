9c60161d4b6e3c7852e898d49ff41d10236aa7bb
ReicaD <dbamanya447@gmail.com>
2025-01-30 19:10:30 -0500

    modified with jsx

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const todoForm = document.querySelector("form");
-  const todoInput = document.getElementById("todo-input");
-  const todoListUL = document.getElementById("todo-list");
-
-  let allTodos = [];
-  // This is for the submit event
-  todoForm.addEventListener("submit", function (e) {
-    e.preventDefault(); // Prevent the default form submission
-    addTodo();
-  });
-
-  function addTodo() {
-    const todoText = todoInput.value.trim();
-    //to present user from submiting an empy todo
-    if (todoText.length > 0) {
-      allTodos.push(todoText);
-      updateTodoList();
-      todoInput.value = "";
+    const todoForm = document.querySelector("form");
+    const todoInput = document.getElementById("todo-input");
+    const todoListUL = document.getElementById("todo-list");
+  
+    let allTodos = [];
+  
+    // This is for the submit event
+    todoForm.addEventListener("submit", function (e) {
+      e.preventDefault(); // Prevent the default form submission
+      addTodo();
+    });
+  
+    function addTodo() {
+      const todoText = todoInput.value.trim();
+      // Prevent user from submitting an empty todo
+      if (todoText.length > 0) {
+        allTodos.push(todoText);
+        updateTodoList();
+        todoInput.value = "";
+      }
     }
-    // alert(todoText);
-    //  console.log(allTodos);
-  }
-  //updating the submited value of the todo list
-  function updateTodoList((todo, todoIndex)=>{
-    todoListUL.innerHTML = "";
-    allTodos.forEach(()=>{
-    let todoItem = createTodoItem(todo, todoIndex);
-    todoListUL.append(todoItem);
-    })
-
-  }
-  function createTodoItem(todo) {
-    const todoLI = document.createElement("li");
-    todoLI.innerText = todo;
-    // todoListUL.append(todoLI);
-    return todoLI;
-  }
-});
+  
+    // Updating the submitted value of the todo list
+    function updateTodoList() {
+      todoListUL.innerHTML = "";
+      allTodos.forEach((todo, todoIndex) => {
+        let todoItem = createTodoItem(todo, todoIndex); // Pass both parameters
+        todoListUL.append(todoItem);
+      });
+    }
+  
+    function createTodoItem(todo, todoIndex) {
+      const todoLI = document.createElement("li");
+      todoLI.innerText = `${todo} (Task #${todoIndex + 1})`; // Example usage of todoIndex
+      return todoLI;
+    }
+  });
+  
